b1f3d510ca2d1848c2cbfeb01d3fbc88c0041696
cpoakley1432 <cpoakley1432@gmail.com>
2015-10-15 10:37:28 -0400

    add warmup and contact-saver

---
+++ src/ExerciseAnswer.java
@@ -0,0 +1,17 @@
+/**
+ * Created by cameronoakley on 10/15/15.
+ */
+public class ExerciseAnswer {
+    public String greeting;
+
+    public ExerciseAnswer(){
+
+    }
+
+    public ExerciseAnswer(String answer){
+        this.greeting = "hello";
+    }
+    public String getGreeting (){
+        return greeting;
+    }
+}
