45fefc318fafe53bf5cdfa050500502a62f0661a
cpoakley1432 <cpoakley1432@gmail.com>
2015-10-19 11:15:55 -0400

    random generator/ exercise 3

---
+++ src/Exercise3.java
@@ -0,0 +1,37 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+
+/**
+ * Created by cameronoakley on 10/19/15.
+ */
+public class Exercise3 {
+    public static void main(String[] args) {
+        String[] names = {"Alice", "Bob", "Charlie", "David"};
+        HashMap<String, ArrayList<Contact>> people = new HashMap();
+
+        for (String name : names) {
+            for (String name2 : names) {
+                if (!names.equals(name2)) {
+                    Contact c = new Contact (name2, 0 );//create contact object
+                    ArrayList<Contact> list = people.get(name);
+                    if(list == null){
+                        list = new ArrayList();
+                        list.add(c);
+                        people.put(name, list);
+                    } else{
+                        list.add(c);
+                    }
+                }
+            }
+        }
+        double randNum = Math.random();
+        int index = (int) (randNum * 4);
+        System.out.println(names [index]);
+
+        Random rand = new Random();
+        index = rand.nextInt(4);
+        System.out.println(names[index]);
+    }
+}
